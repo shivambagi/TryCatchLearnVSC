55cb378a1474bcd3c8dcb906a72b9931d3a948b4
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-08-30 15:03:15 +0530

    End of Section 2

---
+++ Models/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
