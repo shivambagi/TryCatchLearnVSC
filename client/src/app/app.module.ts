ecfde760017a691b5e16db2819777b241466b608
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-09-02 12:34:14 +0530

    60 - UI - Adding a register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NavComponent } from './nav/nav.component';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
