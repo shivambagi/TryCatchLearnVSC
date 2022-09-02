6bf69e79b46a53159bb09ed00f0b7de07ffcbdb8
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-09-02 12:44:46 +0530

    61 - UI - Parent to child communication using @Input(From Home to Register)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,37 +10,16 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  users: any;
 
-  constructor(private http : HttpClient,private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
 
   ngOnInit(){
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers(){
-    // this.http.get('https://localhost:5001/api/users').subscribe(response => {
-    //   this.users = response
-    //   console.log(response);
-    // },error => {
-    //   console.log(error);
-    // })
-
-    //the above is deprecated way
-    
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => {
-        this.users = response 
-        console.log(response)
-      },
-      error: error => console.log(error)
-    })
   }
   
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users:any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+  getUsers(){
+    // this.http.get('https://localhost:5001/api/users').subscribe(response => {
+    //   this.users = response
+    //   console.log(response);
+    // },error => {
+    //   console.log(error);
+    // })
+
+    //the above is deprecated way
+    
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => {
+        this.users = response 
+        console.log(response)
+      },
+      error: error => console.log(error)
+    })
+  }
+
+
 
 }
