a6526847b83202c689d3d0de72b28ff4ed143e43
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-09-02 12:16:39 +0530

    59 - UI - Adding a home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
