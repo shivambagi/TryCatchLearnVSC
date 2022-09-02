b8b56ef986115192f842b534020db9efc194a854
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-09-02 12:05:26 +0530

    58 - UI - Using async pipe

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,12 +10,10 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean;
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
   login() {
@@ -21,7 +21,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: response => { 
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     });    
@@ -29,15 +28,7 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user; // double exclaimation means if it is null = flase else something = true
-    },error => {
-      console.log(error);
-    })
-  }
 
 }
